00a996d66ade2488e791878cc2ba8be48138ce8d
Pavel <nomerdvadcatpyat@gmail.com>
2021-10-23 00:46:04 +0500

    add dockerfile in client app

--- src/main/client/pages/_app.tsx
+++ src/main/client/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app'
 import 'antd/dist/antd.css'
+import { SWRConfig } from "swr";
+
+const SERVER_URL = process.env.SERVER_URL
+const fetcher = (url: string) => fetch(`${SERVER_URL}${url}`).then(response => response.json())
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+  )
 }
+
 export default MyApp
--- src/main/client/pages/index.tsx
+++ src/main/client/pages/index.tsx
@@ -2,12 +2,8 @@ import type { NextPage } from 'next'
 import {DatePicker, Typography} from "antd";
 import useSWR from 'swr';
 
-const SERVER_URL = 'http://localhost:8080'
-
-const fetcher = (url: string) => fetch(`${SERVER_URL}${url}`).then(response => response.json())
-
 const Home: NextPage = () => {
-  const { data, error } = useSWR('/api/user/', fetcher)
+  const { data, error } = useSWR('/api/user/')
 
     console.log('data', data)
 
