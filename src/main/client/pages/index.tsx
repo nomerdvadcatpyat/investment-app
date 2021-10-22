2043a15da64cc7d88e3dee5fbdef0d5fa11b1ac2
Pavel <nomerdvadcatpyat@gmail.com>
2021-10-23 00:04:34 +0500

    add allowed all cors

--- src/main/client/pages/index.tsx
+++ src/main/client/pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next'
-import {DatePicker} from "antd";
+import {DatePicker, Typography} from "antd";
+import useSWR from 'swr';
 
+const SERVER_URL = 'http://localhost:8080'
+
+const fetcher = (url: string) => fetch(`${SERVER_URL}${url}`).then(response => response.json())
 
 const Home: NextPage = () => {
+  const { data, error } = useSWR('/api/user/', fetcher)
+
+    console.log('data', data)
+
   return (
-      <DatePicker />
+      <Typography.Text>
+        {error ? JSON.stringify(error) : JSON.stringify(data)}
+      </Typography.Text>
   )
 }
 
